e25f61faa91b44548dc3d6844bdfd5760957cba9
Bilolceek <tuxtamurodovbilol@gmail.com>
2022-06-08 12:30:05 +0500

    I'm write code report controller and service

--- src/main/java/com/company/controller/ReportController.java
+++ src/main/java/com/company/controller/ReportController.java
@@ -1,10 +1,15 @@
 package com.company.controller;
 
+import com.company.dto.ReportDTO;
+import com.company.dto.request.ReportRequestDTO;
 import com.company.service.RepoetService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/report")
@@ -12,4 +17,36 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class ReportController {
     private final RepoetService repoetService;
+
+    @ApiOperation(value = "Create report", notes = "Method used for create report", nickname = "Bilol")
+    @PostMapping("")
+    @PreAuthorize("hasAnyRole('USER')")
+    public ResponseEntity<ReportDTO> create(@RequestBody ReportRequestDTO dto){
+        return ResponseEntity.ok(repoetService.create(dto));
+    }
+
+    @ApiOperation(value = "Get report list", notes = "Method used for get report list", nickname = "Bilol")
+    @GetMapping("/list-pagination")
+    @PreAuthorize("hasAnyRole('ADMIN')")
+    public ResponseEntity<PageImpl<ReportDTO>> getListPagination(@RequestParam(value = "page", defaultValue = "0") int page,
+                                                                 @RequestParam(value = "size", defaultValue = "5") int size){
+        return ResponseEntity.ok(repoetService.getListPagination(page, size));
+    }
+
+    @ApiOperation(value = "Get by reportId", notes = "Method used for get by reportId", nickname = "Bilol")
+    @GetMapping("/{reportId}")
+    @PreAuthorize("hasAnyRole('ADMIN')")
+    public ResponseEntity<ReportDTO> getById(@PathVariable("reportId") String reportId){
+        return ResponseEntity.ok(repoetService.getByReportId(reportId));
+    }
+
+    @ApiOperation(value = "Get by profileId", notes = "Method used for get report by profile id", nickname = "Bilol")
+    @GetMapping("/profile/{profileId}")
+    @PreAuthorize("hasAnyRole('ADMIN')")
+    public ResponseEntity<PageImpl<ReportDTO>> getByProfileId(@PathVariable("profileId") String profileId,
+                                                              @RequestParam(value = "page", defaultValue = "0") int page,
+                                                              @RequestParam(value = "size", defaultValue = "5") int size){
+        return ResponseEntity.ok(repoetService.getByProfileId(profileId, page, size));
+    }
+
 }
--- src/main/java/com/company/service/RepoetService.java
+++ src/main/java/com/company/service/RepoetService.java
@@ -1,11 +1,83 @@
 package com.company.service;
 
+import com.company.config.details.CustomProfileDetails;
+import com.company.dto.ReportDTO;
+import com.company.dto.request.ReportRequestDTO;
+import com.company.entity.ProfileEntity;
+import com.company.entity.ReportEntity;
+import com.company.exception.ItemNotFoundException;
 import com.company.repository.ReportRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.*;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class RepoetService {
-    private final ReportRepository repository;
+    private final ReportRepository reportRepository;
+
+    public ReportDTO create(ReportRequestDTO dto) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        CustomProfileDetails customProfileDetails = (CustomProfileDetails) authentication.getPrincipal();
+        ProfileEntity profile = customProfileDetails.getProfile();
+        ReportEntity entity = new ReportEntity();
+        entity.setContent(dto.getContent());
+        entity.setProfileId(profile.getId());
+        reportRepository.save(entity);
+        return toDTO(entity);
+    }
+
+    public PageImpl<ReportDTO> getListPagination(int page, int size) {
+        Pageable pageable = PageRequest.of(page, size, Sort.by(Sort.Direction.DESC, "createdDate"));
+        List<ReportDTO> dtoList = new ArrayList<>();
+        Page<ReportEntity> pageList = reportRepository.findAll(pageable);
+        if (pageList.isEmpty()){
+            return new PageImpl<>(new LinkedList<>());
+        }
+        for (ReportEntity reportEntity : pageList){
+            dtoList.add(toDTO(reportEntity));
+        }
+        return new PageImpl<ReportDTO>(dtoList, pageable, pageList.getTotalElements());
+    }
+
+    public PageImpl<ReportDTO> getByProfileId(String profileId, int page, int size) {
+        Pageable pageable = PageRequest.of(page, size, Sort.by(Sort.Direction.DESC, "createdDate"));
+        List<ReportDTO> dtoList = new ArrayList<>();
+        Page<ReportEntity > pageList = reportRepository.findByProfileId(profileId, pageable);
+        if (pageList.isEmpty()){
+            return new PageImpl<>(new LinkedList<>());
+        }
+        for (ReportEntity reportEntity : pageList){
+            dtoList.add(toDTO(reportEntity));
+        }
+        return new PageImpl<ReportDTO>(dtoList, pageable, pageList.getTotalElements());
+
+    }
+
+    public ReportDTO getByReportId(String reportId) {
+        Optional<ReportEntity> optional = reportRepository.findById(reportId);
+        if (optional.isEmpty()){
+            throw new ItemNotFoundException("Report Not Found");
+        }
+        return toDTO(optional.get());
+    }
+
+    public ReportDTO toDTO(ReportEntity entity){
+        ReportDTO dto = new ReportDTO();
+        dto.setId(entity.getId());
+        dto.setContent(entity.getContent());
+        dto.setProfileId(entity.getProfileId());
+        dto.setCreatedDate(entity.getCreatedDate());
+        return dto;
+    }
+
+
+
 }
